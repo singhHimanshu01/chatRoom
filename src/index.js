bed7e43fbb35fb92374945a3348fe3ceac003a0e
Himanshu singh <himanshusinghsanu@gmail.com>
2023-09-11 13:18:33 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfsH85DaGRDJdzdJvhcwxWShjNlO-sqwI",
+  authDomain: "chat-app-b9efa.firebaseapp.com",
+  databaseURL: "https://chat-app-b9efa-default-rtdb.firebaseio.com",
+  projectId: "chat-app-b9efa",
+  storageBucket: "chat-app-b9efa.appspot.com",
+  messagingSenderId: "589225405212",
+  appId: "1:589225405212:web:1f44af328097e1acbd28d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
